a33dbc7b5fed58e2876fc65376df1200faf9d243
Damir Brekalo <damir.brekalo@trikoder.net>
2015-09-21 13:30:18 +0200

    Set "on item select" mode on init

--- src/fastsearch.js
+++ src/fastsearch.js
@@ -25,9 +25,11 @@
 			this.$el = this.$input.closest( this.options.wrapSelector );
 
 			this.url = this.$el.data('url') || this.options.url || this.$el.attr('action');
+			this.onItemSelect = this.$el.data('on-item-select') || this.options.onItemSelect;
 			this.noResultsText = this.$el.data('no-results-text') || this.options.noResultsText;
 			this.inputIdName = this.$el.data('input-id-name') || this.options.inputIdName;
 			this.apiInputName = this.$el.data('api-input-name') || this.options.apiInputName;
+
 			this.query = null;
 			this.hasResults = false;
 			this.elIsForm = this.$el.is('form');
@@ -311,7 +313,7 @@
 
 		handleItemSelect: function( $item ){
 
-			var selectOption = this.options.onItemSelect,
+			var selectOption = this.onItemSelect,
 				model = (this.itemModels.length && this.itemModels[ this.$resultItems.index($item) ]) || {},
 				format = this.options.responseFormat;
 
@@ -380,12 +382,12 @@
 	$.fastsearch = Fastsearch;
 
 	$.fastsearch.defaults = {
-		'url': null,
-		'wrapSelector': 'form',
-		'responseType': 'JSON',
-		'preventSubmit': false,
+		'wrapSelector': 'form', // fastsearch container defaults to closest form. Provide selector for something other
+		'url': null, // plugin will get data from data-url propery, url option or container action attribute
+		'responseType': 'JSON', // default expected server response type - can be set to html if that is what server returns
+		'preventSubmit': false, // prevent submit of form with enter keypress
 
-		'resultsContClass': 'fs_results',
+		'resultsContClass': 'fs_results', // html classes
 		'resultsOpenedClass': 'fsr_opened',
 		'groupClass': 'fs_group',
 		'itemClass': 'fs_result_item',
@@ -393,12 +395,12 @@
 		'loadingClass': 'loading',
 		'noResultsClass': 'fs_no_results',
 
-		'typeTimeout': 140,
-		'minQueryLength': 2,
+		'typeTimeout': 140, // try not to hammer server with request for each keystroke if possible
+		'minQueryLength': 2, // minimal number of characters needed for plugin to send request to server
 
-		'template': null,
+		'template': null, // your template function
 
-		'responseFormat': {
+		'responseFormat': { // Adjust where plugin looks for data in your JSON server response
 			'url': 'url',
 			'html': 'html',
 			'label': 'label',
@@ -406,16 +408,16 @@
 			'groupItems': 'items'
 		},
 
-		'inputIdName': null,
-		'apiInputName': null,
+		'inputIdName': null, // on item select plugin will try to write selected id from model to this input
+		'apiInputName': null, // by default plugin will post input name as query parameter - you can provide custom one here
 
 		'noResultsText': 'No results found',
-		'onItemSelect': 'follow',
+		'onItemSelect': 'follow', // by default plugin follows selected link - other options available are "fillInput" and custom callback - function($item, model, fastsearchApi)
 
-		'parseResponse': null,
-		'onResultsCreate': null,
-		'onGroupCreate': null,
-		'onItemCreate': null
+		'parseResponse': null, // parse server response with your handler and return processed data - function(response, fastsearchApi)
+		'onResultsCreate': null, // adjust results element - function($allResults, data, fastsearchApi)
+		'onGroupCreate': null, // adjust group element when created - function($group, groupModel, fastsearchApi)
+		'onItemCreate': null // adjust item element when created - function($item, model, fastsearchApi)
 	};
 
 	$.fn.fastsearch = function ( options ) {
